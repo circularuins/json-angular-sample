7ec5c4b0b1937ddaa1df72881625409dc4e8879b
wake <wake@www1259up.sakura.ne.jp>
2014-02-13 18:00:32 +0900

    index.html

--- src/json_angular_sample/youtube.clj
+++ src/json_angular_sample/youtube.clj
@@ -6,3 +6,11 @@
 (defn get-movies [word]
   (let [response (http/get API {:query-params {:vq word :start-index 1 :max-results 30 :alt "json" :v 2} :as :json})]
     (((response :body) :feed) :entry)))
+; 【json取得URL】http://circularuins.com:3002/api/movies/検索ワード
+; ここで取れるのはbody.feed.entryまで
+; 以下はangular側で取りたい
+; title
+; link[0].href
+; yt$statistics.viewCount
+; yt$statistics.favoriteCount
+; gd$rating.average
