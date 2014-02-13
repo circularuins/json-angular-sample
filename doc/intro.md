c5f8923b2c4dc12dbcb747c63a5ec8853e4bddc4
wake <wake@www1259up.sakura.ne.jp>
2014-02-13 13:29:04 +0900

    Initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to json-angular-sample
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/json_angular_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns json-angular-sample.core-test
+  (:require [clojure.test :refer :all]
+            [json-angular-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
